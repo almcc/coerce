4c448335a9bd9723d8a627546102935b6e818e27
Alastair McClelland <alastair.mcclelland@gmail.com>
2019-03-26 14:38:26 +0000

    Travis

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from distutils.core import setup
 
-setup(name="coerce", version="0.0.0", packages=["coerce"])
+setup(
+    name="coerce",
+    version="0.0.0",
+    packages=["coerce"],
+    extras_require={"dev": ["pytest", "black"]},
+)
