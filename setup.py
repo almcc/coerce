75cf8f365db65edf423310883a74f8b910818388
Alastair McClelland <alastair.mcclelland@gmail.com>
2019-03-26 15:09:58 +0000

    Bump version to 0.0.1

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from distutils.core import setup
 
 setup(
     name="coerce",
-    version="0.0.0",
+    version="0.0.1",
     packages=["coerce"],
     extras_require={
-        "dev": ["pytest", "pytest-cov", "pytest-black", "black==18.9b0", "coveralls"]
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "pytest-black",
+            "black==18.9b0",
+            "coveralls",
+            "twine",
+        ]
     },
 )
