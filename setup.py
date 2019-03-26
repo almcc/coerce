c7b157118c3462ff8495b94523e262f2c078d6dc
Alastair McClelland <alastair.mcclelland@gmail.com>
2019-03-26 14:49:44 +0000

    coveralls

--- setup.py
+++ setup.py
@@ -4,5 +4,13 @@ setup(
     name="coerce",
     version="0.0.0",
     packages=["coerce"],
-    extras_require={"dev": ["pytest", "pytest-cov", "pytest-black", "black==18.9b0"]},
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "pytest-black",
+            "black==18.9b0",
+            "coveralls==1.6.0",
+        ]
+    },
 )
