285be954820a9c917ad29b3d82ae51c3c46acd4f
Alastair McClelland <alastair.mcclelland@gmail.com>
2019-03-27 19:05:59 +0000

    Improvmentents to pypi release, bump to 0.0.3a1

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="coerce",
-    version="0.0.1",
+    version="0.0.3a1",
+    author="Alastair McClelland",
+    author_email="almcc.dev@gmail.com",
+    description="A type hint coercion decorator.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/almcc/coerce",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages=["coerce"],
     extras_require={
         "dev": [
-            "pytest",
-            "pytest-cov",
-            "pytest-black",
             "black==18.9b0",
             "coveralls",
-            "twine",
             "invoke",
+            "pytest",
+            "pytest-black",
+            "pytest-cov",
+            "setuptools>=40.8.0",
+            "twine",
+            "wheel>=0.33.1",
         ]
     },
 )
